cdea0681a8123da3b9b79d11d9cf390f687b0150
Aaron Spurlock <spurlockat@gmail.com>
2021-06-23 14:07:53 -0400

    init

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# redis-hermes
-
-![version](https://img.shields.io/badge/npm-v0.0.1-blue)
-
-Queue handler for redis messaging with Bull
+# to-do: put shields badges on this
+# to-do: have a decent description instead of... nothing!
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+//         ________
+//        / \\\\\\\\
+//       / # \\\\\\\\
+//      ( * > )REDIS))
+//       \ O /HERMES/
+//        \_////////
+//
+
+import { Queue, Worker, QueueEvents, Job } from 'bullmq'
+import chalk from 'chalk'
+
+//Init queue-events logging
+const queueEvents = new QueueEvents('queue-one')
+
+//Create queue, connect to Redis
+const Q = new Queue('queue-one', { 
+  connection: { 
+    port: 6379, 
+    host: '127.0.0.1'
+  },
+  defaultJobOptions: {
+    attempts: 3,
+    backoff: {
+      type: "exponential",
+      delay: 1000
+    }
+  }
+})
+
+const worker = new Worker("queue-one", async job => {
+  // What will the worker do when processing is complete?
+  console.log(job.data)
+})
+
+//Listen to all worker-events in a given client
+queueEvents.on('waiting', ({ jobId }) => {
+  console.log(`Job with id ${jobId} waiting in the queue...`)
+})
+queueEvents.on('completed', ({ jobId, returnvalue }) => {
+  console.log(`Job with id ${jobId} ${chalk.green('COMPLETED')}, returned as ${chalk.blue(returnvalue)}`)
+})
+queueEvents.on('active', ({ jobId, prev }) => {
+  console.log(`Job with id ${jobId} is now ${chalk.yellow('active')}, previous status was ${chalk.yellow(prev)}`)
+})
+queueEvents.on('progress', ({ jobId, data }, timestamp) => {
+  console.log(`${jobId} reported progress ${data} at ${timestamp}`)
+})
+queueEvents.on('failed', ({ jobId, failedReason }) => {
+  console.log(`Job tagged ID ${jobId} ${chalk.red('FAILED')} with err: ${failedReason}`)
+})
+
+async function JobGroupOne() {
+  await Q.add('Job1', {
+    test_payload_01: {
+      "name" : { "first" : "Billy", "last" : "Wells" },
+      "code" : "314000000X",
+      "isPrimary" : true
+    },
+    test_payload_02: {
+      "name" : { "first" : "Freddy", "last" : "Fred" },
+      "code" : "348000000X",
+      "isPrimary" : false,
+      "desc" : "Facility"
+    }
+  })
+}
+
+await JobGroupOne()
+
+
+
